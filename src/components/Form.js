8331b9c1c7ac408d38cae01fee1746a5bc2861b6
benpaquier <hello@benpaquier.com>
2021-11-05 16:32:35 +0100

    Create Form component

---
+++ src/components/Form.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Form extends Component {
+  render() {
+    return (
+      <form>
+        my super form
+      </form>
+    );
+  }
+}
+
+export default Form;
